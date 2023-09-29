ca71ffbe9212dbe1bc4c6484629ebedea5a42f88
Tinonino <ahgalonso@gmail.com>
2023-09-29 23:19:43 +0200

    First commit

---
+++ recomendation-service/src/main/java/com/tinonino/microservices/core/product/recomendationservice/RecomendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tinonino.microservices.core.product.recomendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationServiceApplication.class, args);
+	}
+
+}
